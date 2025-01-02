8bcd370cd77d6f307a4184fb19e19bbce27f4045
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-02 22:30:40 +0800

    improved draft

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -10,13 +10,12 @@ class Board
   end
  
 end
-arr = [[, ,]...[, ,]]
-arr.each_slice(6).to_a
-arr.each_with_object([]){|value,items|items.push(value)}
+array = Array.new(5, [[]])
+p array 
 #  make a 2d nested array that acts as the board
 #  each element in array has an array in it which contains
 #  [, blank, ] 
-# 
+#  
 # 
 # 
 # 
