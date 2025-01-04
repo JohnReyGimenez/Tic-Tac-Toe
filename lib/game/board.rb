c408736e23714a6f96e15f4e14cbb54574ed199b
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-04 22:49:00 +0800

    revised draft

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -1,25 +1,22 @@
 class Board 
-
   def initialiaze
-    @rows = 7
-    @columns = 7
-    @board = []
-
-
-
+   
   end
  
 end
 
-arr = Array.new(9, [])
-board = arr.each_slice(3){|cell| p cell}
-
-
-
+arr = Array.new(9, [ ])
+board = arr.each_slice(3)
+p board
 #  make a 2d nested array that acts as the board
 #  each element in array has an array in it which contains
 #  [, blank, ] 
-#  
-# 
+
+
+#      
+# 1 [[ ], [ ], [ ]]
+# 2 [[ ], [ ], [ ]]
+# 3 [[ ], [ ], [ ]]
+#     A    B    C
 # 
 # 
