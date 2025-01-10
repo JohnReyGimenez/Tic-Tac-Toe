c37d233cd9f731ee8700cc63210e7f06d5f585aa
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-10 21:19:43 +0800

    refined draft and pseudo code

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -5,9 +5,9 @@ class Board
  
 end
 
-arr = Array.new(9, [ ])
-board = arr.each_slice(3)
-p board
+arr = Array.new(9, "")
+
+p arr
 #  make a 2d nested array that acts as the board
 #  each element in array has an array in it which contains
 #  [, blank, ] 
