4963a656b2d62e70eaaf70e4b38667cafe0876bc
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-11 18:51:12 +0800

    added display instructions fucntion

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -3,6 +3,17 @@ class Board
     @board = Array.new(8, "") #create a board with 9 nil elements
   end
   
+  def display_instructions
+    puts "Shown below are the coordinates of the cells where you will put your tokens"
+    puts "to put your token(X/O) in a cell simply type the number where you wanna put it"
+    puts " 1 | 2 | 3 "
+    puts "-----------"
+    puts " 4 | 5 | 6 "
+    puts "-----------"
+    puts " 7 | 8 | 9 "
+    puts "Type y if you want to start"
+  end
+
   def display_board(board)
     puts "  #{board[0]} |  #{board[1]} |  #{board[2]}"
     puts "-----------"
@@ -14,16 +25,11 @@ class Board
  
 end
 
-
-#  make a 2d nested array that acts as the board
-#  each element in array has an array in it which contains
-
-# 
-# The game has 2 tokens which are X and O
-# In the first turn the X token is used
-# the board is defined by a 2d array which 
-# each element will represent a cell in the board
-# there are 8 winning conditions to be defined
-# to define a win we need to check if the specific indices
-# correlates to the winning combination
-# if there are no winning condition then its a tie/draw
+puts "Shown below are the coordinates of the cells where you will put your tokens"
+puts "to put your token(X/O) in a cell simply type the number where you wanna put it"
+puts " 1 | 2 | 3 "
+puts "-----------"
+puts " 4 | 5 | 6 "
+puts "-----------"
+puts " 7 | 8 | 9 "
+puts "Type y if you want to start"
