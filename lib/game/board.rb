f0312754cf5d2200821c0a37b556d7ecb307caa9
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-11 18:41:36 +0800

    adjusted the board

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -1,31 +1,23 @@
 class Board 
-
   def initialiaze
     @board = Array.new(8, "") #create a board with 9 nil elements
   end
   
   def display_board(board)
-    puts "#{board[0]} | #{board[1]} | #{board[2]}"
-    puts "------------"
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
-    puts "------------"
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+    puts "  #{board[0]} |  #{board[1]} |  #{board[2]}"
+    puts "-----------"
+    puts "  #{board[3]} |  #{board[4]} |  #{board[5]}"
+    puts "-----------"
+    puts "  #{board[6]} |  #{board[7]} |  #{board[8]}"
   end
   
-  display_board(@board)
+ 
 end
 
 
 #  make a 2d nested array that acts as the board
 #  each element in array has an array in it which contains
-#  [, blank, ] 
 
-
-#      
-# 1 [[ ], [ ], [ ]]
-# 2 [[ ], [ ], [ ]]
-# 3 [[ ], [ ], [ ]]
-#     A    B    C
 # 
 # The game has 2 tokens which are X and O
 # In the first turn the X token is used
