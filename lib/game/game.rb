0f3b047a0d49254eaf5bb026b493c45e74600835
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-12 20:37:04 +0800

    added position taken function

---
+++ lib/game/game.rb
@@ -0,0 +1,16 @@
+class Game
+  def initialiaze
+    
+  end
+
+  
+end
+
+# The game has 2 tokens which are X and O
+# In the first turn the X token is used
+# the board is defined by a 2d array which 
+# each element will represent a cell in the board
+# there are 8 winning conditions to be defined
+# to define a win we need to check if the specific indices
+# correlates to the winning combination
+# if there are no winning condition then its a tie/draw or when it reaches 9 turns
