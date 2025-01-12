53d729448673fe6c4f5c657cbf8226c0f90bf5e0
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-12 20:52:17 +0800

    added valid move? function

--- lib/game/player.rb
+++ lib/game/player.rb
@@ -10,7 +10,17 @@ class Player
     board[index] = player
   end
 
-  
+  def position_taken?(board, index)
+    if (board[index] == "") || (board[index] == '') || (board[index] == nil )
+      return true
+    else 
+      return false
+    end
+
+  def valid_move?(board, index)
+    if (board.between(0,8)) && !position_taken(board, index)\
+      return true
+  end
 end
 
 
