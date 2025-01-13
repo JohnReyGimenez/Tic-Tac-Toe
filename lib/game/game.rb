606a68f8bd5a532caf00b0b84c3da91d0d9529ec
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-13 20:15:18 +0800

    added instance variable winning combination

--- lib/game/game.rb
+++ lib/game/game.rb
@@ -1,8 +1,18 @@
 class Game
   def initialiaze
-    
+    @winning_combination = [
+      [0,1,2], #top row
+      [3,4,5], #middle row
+      [6,7,8], #bottom row 
+      [0,3,6], #1st column
+      [1,4,7], #2nd column
+      [2,5,8], #3rd column
+      [0,4,7], #left diagonal
+      [2,4,6], #right diagonal
+    ]
   end
-
+ 
+  
   
 end
 
