254856cef0dbfc93ad34d2e9c798a9c6b80998a3
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-20 18:09:55 +0800

    fixed heredocs

--- lib/game/board.rb
+++ lib/game/board.rb
@@ -4,7 +4,8 @@ class Board
   end
   
   def display_instructions
-     <<HEREDOC Shown below are the coordinates of the cells where you will put your tokens
+     <<-HEREDOC 
+     Shown below are the coordinates of the cells where you will put your tokens
      to put your token(X/O) in a cell simply type the number where you wanna put it
       1 | 2 | 3 
      -----------
@@ -12,7 +13,7 @@ class Board
      -----------
       7 | 8 | 9 
      Type y if you want to start
-    HEREDOC
+HEREDOC
 
   
     start = gets.chomp
