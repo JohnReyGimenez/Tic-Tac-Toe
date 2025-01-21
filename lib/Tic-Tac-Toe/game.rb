02be40058306a9d6dd1ddb7858dd5d8fecd9db7a
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-21 20:52:09 +0800

    added fraw? function

--- lib/Tic-Tac-Toe/game.rb
+++ lib/Tic-Tac-Toe/game.rb
@@ -43,9 +43,19 @@ class Game
     end
     false
   end
+  
+  def full?(board)
+    board.all? {|i| i == "X" || i == "O"}
+  end
 
   def draw?(board)
-    
+    if !won?(board) && full?(board)
+      return true
+    elsif !won?(board) && !full?(board)
+      return false
+    else won?(board)
+      return false
+    end
   end
 end
 
