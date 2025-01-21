d3d6167959dbb9622ea9fc6184ff3e7c5184e77f
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-21 21:46:56 +0800

    fixed capitalization

--- lib/Tic-Tac-Toe.rb
+++ lib/Tic-Tac-Toe.rb
@@ -2,7 +2,7 @@ require_relative 'Tic-Tac-Toe/board'
 require_relative 'Tic-Tac-Toe/game'
 require_relative 'Tic-Tac-Toe/player'
 
-board = board.new
-game = game.new
+board = Board.new
+game = Game.new
 board.display_instructions
 game.play_game(board)
