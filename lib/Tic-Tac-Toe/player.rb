1a56aedd1b49969233e54111cfa58e8d6f1a435e
JohnReyGimenez <gimenezjr5@gmail.com>
2025-01-26 18:35:35 +0800

    removed move function from player

--- lib/Tic-Tac-Toe/player.rb
+++ lib/Tic-Tac-Toe/player.rb
@@ -11,14 +11,6 @@ class Player
   def position_taken?(board, index)
     board[index] != " "
   end
-
-  def valid_move?(board, index)
-    (board.between?(0, 8)) && !position_taken(board, index)
-  end
-
-  def move (board, index, player)
-    board[index] = player
-  end
     
   def turn_count(board) 
     board.count {|space| space == "X"|| space == "O"}
